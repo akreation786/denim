b7953a9f33d7a88591d3c5c4887cc408ea6f9463
akreation786 <akreation786@gmail.com>
2023-08-07 04:26:44 +0600

    ready without responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@
     $('.testimonial_slider').slick({
       infinite: true,
       slidesToShow: 3,
-      slidesToScroll: 3,
+      slidesToScroll: 1,
       prevArrow: '<span class="prev_arrow"><i class="fa-solid fa-arrow-left-long"></i></span>',
       nextArrow: '<span class="next_arrow"><i class="fa-solid fa-arrow-right-long"></i></span>',
     });
