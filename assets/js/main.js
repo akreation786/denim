c2909ded20e8c6515e4801e53299b5a186138670
akreation786 <akreation786@gmail.com>
2023-08-08 02:13:21 +0600

    Fully Responsive done

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,9 +12,38 @@
       slidesToScroll: 1,
       prevArrow: '<span class="prev_arrow"><i class="fa-solid fa-arrow-left-long"></i></span>',
       nextArrow: '<span class="next_arrow"><i class="fa-solid fa-arrow-right-long"></i></span>',
+      responsive: [
+        {
+          breakpoint: 1023,
+          settings: {
+            slidesToShow: 2,
+          }
+        },    
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 2,
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
     });
 
   });
 
 
 })(jQuery);
+
+
